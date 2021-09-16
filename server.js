596ad09104220f96b8a75f06ae076239fcd4da60
shubhamsrinet <shubhamarjunsingh@gmail.com>
2021-09-16 06:25:31 +0000

    updated code

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ console.log(`process.env.SERVER = ${process.env.SERVER}`);
 // get the environment variable, but default to localhost:8082 if its not set
 const SERVER = process.env.SERVER ? process.env.SERVER : "http://localhost:8082";
 
+//testing new branch
 //branch 1 changes
 // express is a nodejs web server
 // https://www.npmjs.com/package/express
